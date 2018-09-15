b3a8fcb3bde43fc058ee5b0bddd7123b797c7c4e
tattimus <samu.tatti@luukku.com>
2018-09-15 18:15:59 +0300

    Addition of ratings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'breweries#index'
+  get 'kaikki_bisset', to: 'beers#index'
+  get 'ratings', to: 'ratings#index'
   resources :beers
   resources :breweries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
