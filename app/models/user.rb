1cfa80d18fa259c0a62edefe9d9bd4e6f23283c4
tattimus <samu.tatti@luukku.com>
2018-10-02 23:12:33 +0300

    Addition of favorite brewery and style to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,9 +16,29 @@ class User < ApplicationRecord
     ratings.order(score: :desc).limit(1).first.beer
   end
 
+  def average_of(ratings)
+    ratings.sum(&:score).to_f / ratings.count
+  end
+
   def favorite_style
+    return nil if ratings.empty?
+
+    style_ratings = ratings.group_by{ |r| r.beer.style }
+    averages = style_ratings.map do |style, ratings|
+      { style: style, score: average_of(ratings) }
+    end
+
+    averages.max_by{ |r| r[:score] }[:style]
   end
 
   def favorite_brewery
+    return nil if ratings.empty?
+
+    style_ratings = ratings.group_by{ |r| r.beer.brewery }
+    averages = style_ratings.map do |brewery, ratings|
+      { brewery: brewery, score: average_of(ratings) }
+    end
+
+    averages.max_by{ |r| r[:score] }[:brewery]
   end
 end
