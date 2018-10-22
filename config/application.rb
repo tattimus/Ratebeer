248d800c176a8f70ce0c9b19d8872f1e34de640f
tattimus <samu.tatti@luukku.com>
2018-10-22 21:42:49 +0300

    fixing precompile

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Ratebeer
     config.load_defaults 5.2
     config.autoload_paths << Rails.root.join("lib")
     config.eager_load_paths << Rails.root.join("lib")
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
